bb92ed7c6a6de10f9ea473ea2589a3fef17cff38
Boddu Venkata Sai Ganesh <40751093+saiganesh1212@users.noreply.github.com>
2020-12-07 13:29:48 +0530

    first commit

---
+++ Repository/IAuthRepo.cs
@@ -0,0 +1,13 @@
+﻿using AuthService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthService.Repository
+{
+    public interface IAuthRepo
+    {
+        public User Login(User user);
+    }
+}
